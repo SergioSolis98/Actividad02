8a3e2453a0b03db7a280337419729b6252084f21
sergio solis diaz <troy_polamalu98@hotmail.com>
2025-01-27 07:59:13 -0600

    cambio2

--- Actividad02/Actividad02.cpp
+++ Actividad02/Actividad02.cpp
@@ -4,13 +4,14 @@ const int NUM = 8;
 int main()
 {
 	int nums[NUM];
-	int total;
+	int total=0;
 	for (int i = 0;i < NUM;i++)
-	{
+	    {
 		cout << "Por favor, introduzca el número";
 		cin >> nums[i];
 		total += nums[i];
-	}
+	    }
 	cout << "El total de números es" << total << endl;
 	system("pause");
-
+	return 0;
+}
